7690b229266c8bb4506287a4806b1cfcaa1eec06
Lee-412 <115365338+Lee-412@users.noreply.github.com>
2023-02-25 16:39:36 +0700

    Add files via upload

---
+++ note.cpp
@@ -0,0 +1,14 @@
+
+void waitUntilKeyPressed();
+
+void waitUntilKeyPressed()
+{
+    //SDL_Event e;
+    while (true) {
+        if ( SDL_WaitEvent(&e) != 0 &&
+             (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) )
+            return;
+        SDL_Delay(100);
+    }
+}
+
